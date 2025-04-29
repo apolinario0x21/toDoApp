235c4ed69fee9618ae3d034ec08047d924f447cf
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-04-29 04:56:13 -0300

    feat: add service Task

---
+++ src/main/java/apolinario0x21/toDoApp/service/TaskService.java
@@ -0,0 +1,29 @@
+package apolinario0x21.toDoApp.service;
+
+import apolinario0x21.toDoApp.model.Task;
+import apolinario0x21.toDoApp.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+        // elemento existe?
+    }
+
+
+}
