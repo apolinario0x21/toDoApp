4d65f08afa44207e152219efa8d99f118481ec3b
apolinario0x21 <imarcelo.apolinario@gmail.com>
2025-08-07 21:13:33 -0300

    feat: implementa logging SLF4J e bugs de builder docker fixados

--- src/main/java/apolinario0x21/toDoApp/service/TaskService.java
+++ src/main/java/apolinario0x21/toDoApp/service/TaskService.java
@@ -6,6 +6,8 @@ import apolinario0x21.toDoApp.exceptions.TaskNotFoundException;
 import apolinario0x21.toDoApp.model.Task;
 import apolinario0x21.toDoApp.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import java.util.UUID;
 
 @Service
 public class TaskService {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskService.class);
 
     @Autowired
     private TaskRepository taskRepository;
@@ -29,48 +33,74 @@ public class TaskService {
 
     public TaskResponseDTO createTask(TaskRequestDTO request) {
 
+        logger.info("Criando uma nova tarefa: {}", request.title());
+
         Task task = new Task();
         task.setTitle(request.title());
         task.setCompleted(false);
         Task savedTask = taskRepository.save(task);
+
+        logger.info("Tarefa criada com sucesso: {}", savedTask.getId());
         return toResponse(savedTask);
     }
 
     public List<TaskResponseDTO> findAllTasks() {
+
+        logger.info("Buscando todas as tarefas");
+
         return taskRepository.
                 findAllByOrderByCreatedAtDesc()
                 .stream().map(this::toResponse).toList();
     }
-    //getTaskById(UUID id)
 
     private Task findById(UUID id) {
         return taskRepository.findById(id)
-                .orElseThrow(() -> new TaskNotFoundException("Task not found. ID:" + id));
+                .orElseThrow(() -> {
+
+                    logger.error("Tarefa não encontrada. ID: {}", id);
+                    return new TaskNotFoundException("Tarefa não encontrada. ID: " + id);
+                });
     }
 
     public TaskResponseDTO findTaskById(UUID id) {
+
+        logger.info("Buscando tarefa por ID: {}", id);
+
         Task task = findById(id);
         return toResponse(task);
     }
 
     public void titleUpdateTask(UUID id, String newTitle) {
+
+        logger.info("Atualizando o titulo da tarefa com ID: {}. Novo título: {}", id, newTitle);
+
         Task task = findById(id);
         task.setTitle(newTitle);
         taskRepository.save(task);
+
+        logger.info("Título da tarefa com ID: {} atualizado com sucesso", id);
     }
 
     public void statusUpdateTask(UUID id, boolean completed) {
+
+        logger.info("Atualizando o status da tarefa com ID {}. Concluída: {}", id, completed);
+
         Task task = findById(id);
         task.setCompleted(completed);
         taskRepository.save(task);
+
+        logger.info("Status da tarefa com ID: {} atualizado com sucesso", id);
     }
 
     public void deleteTask(UUID id) {
+
         if (!taskRepository.existsById(id)) {
+            logger.info("Falha ao excluir. Tarefa não encontrada ID: {}", id);
             throw new TaskNotFoundException("Task not found. ID:" + id);
         }
 
         taskRepository.deleteById(id);
+        logger.info("Tarefa com ID: {} deletada com sucesso", id);
     }
 
 
